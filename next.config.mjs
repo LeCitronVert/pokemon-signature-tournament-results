85a0adafe5074b06fd994e178dafcae212fbdca6
Citron <contact@youceflcv.fr>
2025-02-12 22:09:45 +0100

    surely this fucking time

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-    output: 'export',
-    basePath: "/pokemon-signature-tournament-results",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/pokemon-signature-tournament-results/' : '',
+  basePath: isProd ? '/pokemon-signature-tournament-results' : '',
+  output: 'export'
 };
 
 export default nextConfig;
